eb46fe980ad6e551091995c6322a857b2cd9197d
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-04-05 11:12:56 +1200

    Create 00_LU_base_v1.py

---
+++ 00_LU_base_v1.py
@@ -0,0 +1,43 @@
+"""Lu Base component
+Components added after they have been created and tested"""
+
+
+# Yes/no checking function
+def yes_no(question_text):
+    while True:
+
+        # Ask the user if they have played before
+        answer = input(question_text).lower()
+
+        # If user says yes, output 'Program Continues'
+        if answer == "yes" or answer == "y":
+            answer = "Yes"
+            return answer
+
+        # If user says no, output 'Display Instructions'
+        elif answer == "no" or answer == "n":
+            answer = "No"
+            return answer
+
+        # Otherwise - show error
+        else:
+            print("Please answer 'yes' or 'no' :")
+
+
+# function to display instructions
+def instructions():
+    print("***** How To Play *****")
+    print()
+    print("The rules of the game will go here")
+    print()
+    print("program continues")
+    print()
+
+
+# Main routine go here....
+played_before = yes_no("Have you played this game before? ")
+
+if played_before == "No":
+    instructions()
+else:
+    print("Program continues")
