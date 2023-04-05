e59e3923aed6e03fdf496de17b7c30bf078b649e
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-04-05 20:08:55 +1200

    Update 05_token_generator_v4.py

--- 05_token_generator_v4.py
+++ 05_token_generator_v4.py
@@ -5,24 +5,37 @@ Calculate percentages to ensure the odds favour teh house
 
 import random
 
-tokens = ["Unicorn",
-          "Horse", "Horse", "Horse",
-          "Donkey", "Donkey", "Donkey",
-          "Zebra", "Zebra", "Zebra"]
 STARTING_BALANCE = 100
 balance = STARTING_BALANCE
 
 # Testing loop to generate 20 tokens
 for item in range(20):
-    token = random.choice(tokens)
+    number = random.randint(1, 100)
 
     # adjust balance
-    if token == "Unicorn":
+    # if the random number is between 1 and 5
+    # User gets a unicorn (add $4 to balance)
+    if 1 <= number <= 5:
+        token = "Unicorn"
         balance += 4
-    elif token == "Donkey":
+
+    # if the random number is between 6 and 36
+    # User gets a donkey (subtract $1 from balance)
+    elif 6 <= number <= 36:
+        token = "Donkey"
         balance -= 1
+
+    # in all other cases the token must be a horse or a zebra
+    # (subtract $0.50 from the balance in either case)
     else:
-        balance -= 0.50
+        # If the number is even, set the token Zebra
+        if number % 2 == 0:
+            token = "Zebra"
+            balance -= 0.5
+        # otherwise, set the token to Horse
+        else:
+            token = "Horse"
+            balance -= 0.5
 
     # output
 print(f"Starting balance = ${STARTING_BALANCE:.2f}")
