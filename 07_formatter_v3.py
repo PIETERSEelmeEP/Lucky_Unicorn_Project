41c6114fc83bd51141e0980fca551fb4e9996ca6
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-04-12 18:14:24 +1200

    Create 07_formatter_v3.py

---
+++ 07_formatter_v3.py
@@ -0,0 +1,19 @@
+"""Component 5 - statement formatter v3
+Call function 3 times - one for each test
+"""
+
+
+# function to format text
+def formatter(_symbol, _text):
+    sides = _symbol * 3
+    formatted_text = f"{sides} {_text} {sides}"
+    top_bottom = _symbol * len(formatted_text)
+    return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
+
+
+# main routine
+print(formatter("-", "Welcome to the Lucky Unicorn Game"))
+print()
+print(formatter("!", "Congratulations, you got an Unicorn"))
+print()
+print(formatter("*", "Goodbye"))
