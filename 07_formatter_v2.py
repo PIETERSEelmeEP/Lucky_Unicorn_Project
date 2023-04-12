487f5392910ef8778ffddbb4ab298b3e4ab9a271
PIETERSEelmeEP <elme23pieterse@gmail.com>
2023-04-12 18:14:21 +1200

    Update 07_formatter_v2.py

--- 07_formatter_v2.py
+++ 07_formatter_v2.py
@@ -1,14 +1,18 @@
 """Component 5 - statement formatter v2
-
+Change v1 into a function
 """
 
-symbol = "*"
-text = "hello world"
-sides = symbol * 3
 
-formatted_text = f"{sides} {text} {sides}"
-top_bottom = symbol * len(formatted_text)
+# function to format text
+def formatter(_symbol, _text):
+    sides = _symbol * 3
+    formatted_text = f"{sides} {_text} {sides}"
+    top_bottom = _symbol * len(formatted_text)
+    return f"{top_bottom}\n{formatted_text}\n{top_bottom}"
 
-print(top_bottom)
-print(formatted_text)
-print(top_bottom)
+
+# main routine
+text = input("Enter the statement you want to format: ")
+symbol = input("What symbol do you want to use: ")
+print()
+print(formatter(symbol, text))
