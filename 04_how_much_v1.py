0d6081124251478cc3bf5dd41c055b7b57d57ea4
PIETERSEelmeEP <elme23pieterse@gmail.com>
2024-02-07 13:49:26 +1300

    Update 04_how_much_v1.py

--- 04_how_much_v1.py
+++ 04_how_much_v1.py
@@ -3,7 +3,8 @@ Ask user how much they want to play with and check that the input is a valid
 integer between 1 and 10. If it is, this amount becomes the balance in their
 account"""
 
-user_balance = int(input("How much do you want to play with (must be an integer between 1 and 10) $"))
+user_balance = int(input("How much do you want to play with (must be an "
+                         "integer between 1 and 10) $"))
 
 # Keep asking until a valid amount is entered
 while not 1 <= user_balance <= 10:
